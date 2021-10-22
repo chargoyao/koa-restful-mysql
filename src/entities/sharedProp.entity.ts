feaa3e4626576de44dfaa05333844e04daf8d9e0
chargo <chargoyao@gmail.com>
2021-10-22 20:16:33 +0800

    feat(add): add

--- src/entities/sharedProp.entity.ts
+++ src/entities/sharedProp.entity.ts
@@ -3,13 +3,13 @@ import { UpdateDateColumn, CreateDateColumn } from 'typeorm';
 export class SharedProp {
   @CreateDateColumn({
     type: 'datetime',
-    name: 'create_at'
+    name: 'create_at',
   })
   createAt!: Date;
 
   @UpdateDateColumn({
     type: 'datetime',
-    name: 'updated_at'
+    name: 'updated_at',
   })
   updateAt!: Date;
 }
